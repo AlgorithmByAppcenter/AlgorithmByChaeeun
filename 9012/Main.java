25b471a1c5529a2fbc20f49a2409251980310cc9
chaeeun <chaeeun218@gmail.com>
2016-05-11 18:51:41 +0900

    no message

--- 9012/Main.java
+++ 9012/Main.java
@@ -3,38 +3,62 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Main {
+public class Main  {
     
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		ArrayList<String> list = new ArrayList<String>();
 		ArrayList<String> answer = new ArrayList<String>();
 		Stack<String> stack = new Stack<String>();
-		int num = scanner.nextInt();
+		String line = scanner.nextLine();
+		int num = Integer.parseInt(line);
+		
 		char[] array = new char[100];
 		
-		for(int i=0; i<num ; i++) {
+		for(int i=0; i<=num ; i++) {
 			String value = scanner.nextLine();
 			list.add(value);
+			System.out.println(value);
 		}
 		for(int i=0; i<list.size() ; i++ ) {
 			array = (list.get(i)).toCharArray();
 
+			
 			for(int j=0; j<array.length; j++) {
 				if(array[j] == '('){
+					try{
 					stack.push("(");
+					}catch(Exception e) {
+					}
 					
 				}else if(array[j] == ')') {
+					try {
 					stack.pop();
+					}catch (Exception e) {
+						
+					}
 				}
 			}
 			
-			if(stack.empty()){
+			try{
+				if(stack.empty()){
+					answer.add("YES");
+					}
+			}catch(Exception e) {
 				answer.add("YES");
-			}else {
+			}
+						
+			try{
+				if(!stack.empty()){
+					answer.add("NO");
+					}
+			}catch(Exception e1) {
 				answer.add("NO");
 			}
 			
+			
+			
+		
 		}
 		for(int i=0; i<answer.size(); i++) {
 
